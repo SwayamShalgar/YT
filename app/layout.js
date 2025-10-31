381515bd27b965c1f001059d192cebebc75ac41b
Swayam Shalgar <swayamshalgar1211@gmail.com>
2025-10-31 20:55:37 +0530

    ads2

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="monetag" content="a87b08c9cb4ad62c9a26764da16f9d1b"></meta>
+      </head>
       <body>{children}</body>
     </html>
   );
