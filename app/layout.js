4967b65e485e16f4b932b4e43273fe73a63e59a7
Swayam Shalgar <swayamshalgar1211@gmail.com>
2025-10-31 21:01:40 +0530

    adsDone

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta name="monetag" content="a87b08c9cb4ad62c9a26764da16f9d1b"></meta>
+        <script src="https://fpyf8.com/88/tag.min.js" data-zone="181909" async data-cfasync="false"></script>
       </head>
       <body>{children}</body>
     </html>
